2f61c9da8f7bd0c122cfa22712ab587362eecff2
ANjitGC741 <anzit741@gmail.com>
2023-01-26 17:10:25 +0545

    change

---
+++ Gameland/Controllers/forHeaderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gameland.Controllers
+{
+    public class forHeaderController : Controller
+    {
+        // GET: forHeader
+        public ActionResult header()
+        {
+            return View();
+        }
+    }
+}
