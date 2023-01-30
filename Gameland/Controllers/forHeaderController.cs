ac876a491b359c3f61a248c34966a90028cc26f9
ANjitGC741 <anzit741@gmail.com>
2023-01-30 23:19:41 +0545

    change

--- Gameland/Controllers/forHeaderController.cs
+++ Gameland/Controllers/forHeaderController.cs
@@ -6,11 +6,19 @@ using System.Web.Mvc;
 
 namespace Gameland.Controllers
 {
+    public class Global : HttpApplication
+    {
+        void Session_Start(object sender, EventArgs e)
+        {
+            Session["userName"] = null;
+        }
+    }
+
     public class forHeaderController : Controller
     {
         // GET: forHeader
         public ActionResult header()
-        {
+        { 
             return View();
         }
     }
