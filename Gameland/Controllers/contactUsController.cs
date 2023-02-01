bb3865d6788e43fca853fe74c03b04bfd247b94b
ANjitGC741 <anzit741@gmail.com>
2023-02-01 19:09:17 +0545

    change

--- Gameland/Controllers/contactUsController.cs
+++ Gameland/Controllers/contactUsController.cs
@@ -7,18 +7,30 @@ using System.Web.Mvc;
 
 namespace Gameland.Controllers
 {
+    public class Global1 : HttpApplication
+    {
+        void Session_Start(object sender, EventArgs e)
+        {
+            Session["message"] = "no";
+        }
+    }
     public class contactUsController : Controller
     {
         forGameLandEntities1 db = new forGameLandEntities1();
         // GET: contactUs
-        public ActionResult contactUsPage()
-        {
+        public ActionResult contactUsPage() {
             return View();
         }
         public ActionResult saveMessage(messageBoxData1 msg)
         {
+            Session["message"] = "yes";
             db.messageBoxData1.Add(msg);
             db.SaveChanges();   
+            return RedirectToAction("contactUsPage");
+        }
+        public ActionResult messageReceived()
+        {
+            Session["message"] = "no";
             return RedirectToAction("contactUsPage");
         }
     }
